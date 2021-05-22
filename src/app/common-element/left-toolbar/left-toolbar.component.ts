31dc98a5b3203834822dd7aa65fd5d36ef832639
DenPham <denpham12197@gmail.com>
2021-05-22 23:55:01 +0700

    den phan quyen menu

--- src/app/common-element/left-toolbar/left-toolbar.component.ts
+++ src/app/common-element/left-toolbar/left-toolbar.component.ts
@@ -1,27 +1,108 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 import {NestedTreeControl} from '@angular/cdk/tree';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { User } from 'src/app/models/user';
 
 interface MenuNode {
+  id: string;
   name: string;
   url: string;
   children?: MenuNode[];
   active: boolean;
 }
 
+// Left bar menu
 const TREE_DATA: MenuNode[] = [
   {
+    id: 'quan-li',
     name: 'Quản lí',
     children: [
       {
+        id: 'user',
+        name: 'User',
+        url: '/user',
+        active: false
+      },
+      {
+        id: 'nhan-vien',
         name: 'Nhân viên',
-        url: '/nhanvien',
-        active: true
+        url: '/nhan-vien',
+        active: false
+      },
+      {
+        id: 'lop',
+        name: 'Lớp',
+        url: '/lop',
+        active: false
+      },
+      {
+        id: 'hoc-sinh',
+        name: 'Học sinh',
+        url: '/hoc-sinh',
+        active: false
+      },
+      {
+        id: 'mon-an',
+        name: 'Món ăn',
+        url: '/mon-an',
+        active: false
+      },
+      {
+        id: 'thuc-pham',
+        name: 'Thực phẩm',
+        url: '/thuc-pham',
+        active: false
       }
     ],
     url: '#',
     active: false
-  }
+  },
+  {
+    id: 'phieu',
+    name: 'Phiếu',
+    url: '#',
+    children: [
+      {
+        id: 'phieu-yeu-cau-thuc-pham',
+        name: 'Phiếu yêu cầu thực phẩm',
+        url: '/phieu/yeu-cau-thuc-pham',
+        active: false
+      },
+      {
+        id: 'phieu-bao-an',
+        name: 'Phiếu báo ăn',
+        url: '/phieu/bao-an',
+        active: false
+      },
+      {
+        id: 'phieu-cung-cap',
+        name: 'Phiếu cung cấp',
+        url: '/phieu/cung-cap',
+        active: false
+      },
+      {
+        id: 'phieu-ban-giao',
+        name: 'Phiếu bàn giao',
+        url: '/phieu/ban-giao',
+        active: false
+      },
+      {
+        id: 'phieu-kiem-ke',
+        name: 'Phiếu kiểm kê',
+        url: '/phieu/kiem-ke',
+        active: false
+      },
+      {
+        id: 'phieu-giao',
+        name: 'Phiếu giao',
+        url: '/phieu/giao',
+        active: false
+      },
+    ],
+    active: false
+  },
+  
 ]
 
 
@@ -31,11 +112,22 @@ const TREE_DATA: MenuNode[] = [
   styleUrls: ['./left-toolbar.component.css']
 })
 export class LeftToolbarComponent implements OnInit {
+  menuGiaoVien: string[] = ['lop', 'phieu-bao-an'];
+  menuNhaBep: string[] =  ['thuc-pham', 'mon-an', 'phieu-yeu-cau-thuc-pham', 'phieu-bao-an', 'phieu-cung-cap', 'phieu-ban-giao', 'phieu-kiem-ke', 'phieu-giao'];
+  menuAdmin: string[] = ['user', 'lop', 'hoc-sinh', 'mon-an', 'thuc-pham', 'phieu-yeu-cau-thuc-pham', 'phieu-bao-an', 'phieu-cung-cap', 'phieu-ban-giao', 'phieu-kiem-ke', 'phieu-giao'];
+  menuMain: string[];
+  currentUser!: User
+
   treeControl = new NestedTreeControl<MenuNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<MenuNode>();
-  constructor() { 
+  constructor(private authenticationService: AuthenticationService) { 
     this.dataSource.data = TREE_DATA;
 
+    // get role
+    this.currentUser = this.authenticationService.userValue;
+    
+    // Complete menu with role
+    this.menuMain = this.getMenuFromRole(this.currentUser.role);
   }
 
   ngOnInit(): void {
@@ -43,4 +135,13 @@ export class LeftToolbarComponent implements OnInit {
 
   hasChild = (_: number, node: MenuNode) => !!node.children && node.children.length > 0;
 
+  getMenuFromRole(role: string) {
+    if (role === 'GiaoVien') {
+      return this.menuGiaoVien;
+    } else if (role === 'NhaBep' || role == 'ThucPham') {
+      return this.menuNhaBep;
+    } else {
+      return this.menuAdmin;
+    }
+  }
 }
